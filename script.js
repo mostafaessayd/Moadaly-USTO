6c758841c14f858359c4f401af9efa40a3645079
mohamedessayd <ballm3957@gmail.com>
2025-12-31 16:14:02 +0100

    new

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
 function calculer() {
 
-    function deux(a, b) {
+    function avg(a, b) {
         return (a + b) / 2;
     }
 
     let total = 0;
-    let coefTotal = 17;
+    let coef = 17;
 
-    total += deux(+and_exam.value, +and_cc.value) * 2;
-    total += deux(+modsim_exam.value, +modsim_cc.value) * 2;
-    total += deux(+roc_exam.value, +roc_cc.value) * 2;
-    total += deux(+aa_exam.value, +aa_cc.value) * 1;
+    total += avg(+and_exam.value, +and_cc.value) * 2;
+    total += avg(+modsim_exam.value, +modsim_cc.value) * 2;
+    total += avg(+roc_exam.value, +roc_cc.value) * 2;
+    total += avg(+aa_exam.value, +aa_cc.value);
 
-    total += +fli_exam.value * 1;
-    total += +tns_exam.value * 1;
+    total += +fli_exam.value;
+    total += +tns_exam.value;
 
-    total += deux(+asr_exam.value, +asr_cc.value) * 2;
+    total += avg(+asr_exam.value, +asr_cc.value) * 2;
 
     let bda = (+bda_exam.value + +bda_td.value + +bda_tp.value) / 3;
     total += bda * 3;
 
-    total += deux(+sfab_exam.value, +sfab_cc.value) * 2;
+    total += avg(+sfab_exam.value, +sfab_cc.value) * 2;
 
-    total += +ethique_cc.value * 1;
+    total += +ethique_cc.value;
 
-    let moyenne = total / coefTotal;
-    resultat.value = moyenne.toFixed(2);
+    resultat.value = (total / coef).toFixed(2);
 }
 
-// حساب مباشر عند تحميل الصفحة
 calculer();
